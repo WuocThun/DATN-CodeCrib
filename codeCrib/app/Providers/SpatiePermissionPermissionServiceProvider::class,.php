2113f34226db3733322443213401f55c338b1897
WuocThun <binngoc1963@gmail.com>
2024-09-27 16:35:54 +0700

    add package spatie permission

---
+++ codeCrib/app/Providers/SpatiePermissionPermissionServiceProvider::class,.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SpatiePermissionPermissionServiceProvider::class, extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
